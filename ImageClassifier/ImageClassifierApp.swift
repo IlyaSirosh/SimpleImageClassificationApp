cc8bdee425a16bf805e2ecebeea2f4f7fa369cf8
Ilya Sirosh <sirosh.ilya@gmail.com>
2021-01-16 06:36:40 +0200

    Initial Commit

---
+++ ImageClassifier/ImageClassifierApp.swift
@@ -0,0 +1,17 @@
+//
+//  ImageClassifierApp.swift
+//  ImageClassifier
+//
+//  Created by Illya Sirosh on 16.01.2021.
+//
+
+import SwiftUI
+
+@main
+struct ImageClassifierApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
